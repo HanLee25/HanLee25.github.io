9578751c252bdfe53c91e18b33b8412d6bfd8410
HanLee25 <eyian25@gmail.com>
2020-06-28 03:31:49 -0400

    Commit test

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
+    title: `Han Lee's Gatsby Starter Tailwind`,
     description: `Gatsby starter styled with Tailwind`,
     author: `@taylorbryant`,
   },
